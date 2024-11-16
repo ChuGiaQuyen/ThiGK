3ee57a9d2a39fb13475df568a521a946a5e94538
ChuGiaQuyen <quyencg.12c3bc.1920@gmail.com>
2024-11-16 10:15:49 +0700

    ham chan

--- func.cpp
+++ func.cpp
@@ -4,6 +4,10 @@ void func() {
 	int a;
 	cin >> a;
 	cout << "Số nguyên vừa nhập là: " << a;
+	if (a % 2 == 0)
+		cout << "Số vừa nhập là số chẵn!";
+	else
+		cout << "Số vừa nhập không là số chẵn!";
 	
 }
 void main() {
